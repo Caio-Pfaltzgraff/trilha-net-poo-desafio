30bfa5671214ed49b347d60b8d976f1fcbbaa8c7
Caio Pfaltzgraff <caio.rllcr@gmail.com>
2024-10-20 14:10:08 -0300

    feat: Desafio realizado
    Iphone e Nokia herdam de Smarphone e é implementado o método de instalarAplicativo em ambas as classes com suas diferenças

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia:");
+Smartphone nokia = new Nokia("12345678", "Tijolão", "111111111111", 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Gmail");
+
+Console.WriteLine();
+
+Console.WriteLine("Iphone:");
+
+Smartphone iphone = new Iphone("87654321", "15 Pro Max", "222222222222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
